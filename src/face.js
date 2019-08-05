3f0ece822d6b1b8e5a42688a15a0e1a1a2edd00d
Xu Zhen <xuzhen_vendor@domain.sensetime.com>
2019-08-05 18:45:38 +0800

    feature: 完成人脸框

--- src/face.js
+++ src/face.js
@@ -11,10 +11,10 @@ export const data = {
       },
       image: {
         height: 1080,
-        width: 1920,
-        url: "http://10.5.6.33:18181/g1/M00/00/CF/CgUGIV06xwOAA4lGAABLrZHQP18572.jpg"
+        url: 'http://10.5.6.33:18181/g1/M01/00/CF/CgUGIV06xwOAJU9iAAIXlN7EVGE970.jpg',
+        width: 1920
       }
-    },
+    }
   }
 
 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,32 @@ import { data } from './face';
 import $ from 'jquery';
 import './index.css';
 
+const imgUrl = data.face.image.url;
+const imgHeight = data.face.image.height;
+const imgWidth = data.face.image.width;
+// width = 1920 height = 1080
+const scale = 2;
+const facePosition = {
+  start: data.face.start,
+  end: data.face.end
+}
+$('.faceImg').attr('src', imgUrl);
+$('.faceImg').width(imgWidth / scale);
+$('.faceImg').height(imgHeight / scale);
+// 人脸框 的长宽
+const faceWidth = (facePosition.end.x - facePosition.start.x) / scale;
+const faceHeight = (facePosition.end.y - facePosition.start.y) / scale;
+console.log(faceWidth, faceHeight);
+var ele = $('<div></div>');
+$(ele).css({
+  "width": faceWidth,
+  "height": faceHeight,
+  "position": "absolute",
+  "left": facePosition.start.x / 2,
+  "top": facePosition.start.y / 2, 
+  "border": "1px solid red"
+})
 
+
+
+$('.face-detect').append(ele);
