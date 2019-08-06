f0c1c3c141dbb5f4020cc1728d9e96bae845f0bf
Xu Zhen <xuzhen_vendor@domain.sensetime.com>
2019-08-06 10:29:07 +0800

    feature: 多人脸识别框

--- src/face.js
+++ src/face.js
@@ -17,4 +17,61 @@ export const data = {
     }
   }
 
+  export const face = [
+    {
+      faceId: 1,
+      mediaUrl: 'http://10.9.242.79:8080/0802/st052/1564464526130.jpg',
+      imgInfo: {
+        width: 1920,
+        height: 1080
+      },
+      vertices: [
+        {
+          x: 1244,
+          y: 362
+        },
+        {
+          x: 1284,
+          y: 402
+        }
+      ]
+    },
+    {
+      faceId: 2,
+      mediaUrl: 'http://10.9.242.79:8080/0802/st052/1564464526130.jpg',
+      imgInfo: {
+        width: 1920,
+        height: 1080
+      },
+      vertices: [
+        {
+          x: 1065,
+          y: 320
+        },
+        {
+          x: 1109,
+          y: 366
+        }
+      ]
+    },
+    {
+      faceId: 3,
+      mediaUrl: 'http://10.9.242.79:8080/0802/st052/1564464526130.jpg',
+      imgInfo: {
+        width: 1920,
+        height: 1080
+      },
+      vertices: [
+        {
+          x: 998,
+          y: 347
+        },
+        {
+          x: 1044,
+          y: 391
+        }
+      ]
+    }
+  ]
+
 
